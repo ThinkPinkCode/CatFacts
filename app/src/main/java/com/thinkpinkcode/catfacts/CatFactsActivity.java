0e9fd5b14a42eaf99db449daa26b92dc44db3e92
Melanie Myers <MMelanieJ@gmail.com>
2018-11-18 16:54:12 -0500

    initial commit

---
+++ app/src/main/java/com/thinkpinkcode/catfacts/CatFactsActivity.java
@@ -0,0 +1,13 @@
+package com.thinkpinkcode.catfacts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CatFactsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cat_facts);
+    }
+}
