fa4d7cbfbbabb94a4fc6b97432ad7416576d511b
Melanie Myers <MMelanieJ@gmail.com>
2018-11-21 16:41:45 -0500

    declare and initialize textview and button

--- app/src/main/java/com/thinkpinkcode/catfacts/CatFactsActivity.java
+++ app/src/main/java/com/thinkpinkcode/catfacts/CatFactsActivity.java
@@ -2,12 +2,28 @@ package com.thinkpinkcode.catfacts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class CatFactsActivity extends AppCompatActivity {
+
+    //Declare our view variables
+    private TextView factTextView;
+    private Button showFactButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cat_facts);
+
+        //Assign views from the layout file to the corresponding variable
+
+        factTextView = findViewById(R.id.factTextView);
+        showFactButton = findViewById(R.id.showFactButton);
+
     }
+
+
+
 }
